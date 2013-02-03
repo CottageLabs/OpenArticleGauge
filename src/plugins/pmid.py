aeb88730a597ba8674697c821dd5733c2c93b590
Richard Jones <richard@cottagelabs.com>
2013-02-03 17:23:57 +0000

    add pmid type detection/validation and associated tests

--- src/plugins/pmid.py
+++ src/plugins/pmid.py
@@ -1,3 +1,6 @@
+import re
+import models
+
 def type_detect_verify(bibjson_identifier):
     """
     determine if the provided bibjson identifier has a type of "PMID", by
@@ -5,8 +8,33 @@ def type_detect_verify(bibjson_identifier):
     of the id.  If it is a PMID, then verify that it is a valid one.
     
     Add "type" : "pmid" to the bibjson_identifier object if so
+    
+    NOTE: PMIDs could come prefixed with a bunch of URL spaces, but we don't really
+    have an exhaustive list of these, so for the time being this method will FAIL
+    to identify any PMID which is not just an 8 digit number
     """
-    pass
+    if bibjson_identifier.has_key("type") and bibjson_identifier["type"] != "pmid":
+        return
+    
+    if not bibjson_identifier.has_key("id"):
+        return
+    
+    # 8 digits long
+    rx = "^[\d]{8}$"
+    result = re.match(rx, bibjson_identifier["id"])
+    
+    # validation
+    if bibjson_identifier.has_key("type") and bibjson_identifier["type"] == "pmid" and result is None:
+        # the bibjson identifier asserts that it is a pmid, but the regex does not
+        # support this assertion, so we raise an exception
+        raise models.LookupException("identifier asserts it is a PMID, but cannot validate: " + str(bibjson_identifier["id"]))
+    
+    if result is None:
+        # no assertion that this is a PMID, and no confirmation from the regex
+        return
+    
+    # otherwise, this is confirmed as a PMID
+    bibjson_identifier["type"] = "pmid"
 
 def canonicalise(record):
     """
