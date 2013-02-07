1077f957a93a07229bbbef56f97a6fd42fb5cce7
Emanuil Tolev <emanuil.tolev@gmail.com>
2013-02-07 09:52:45 +0000

    first license plugin - attempt to get CC-BY 2.0 in BMC articles

--- src/plugins/bmc.py
+++ src/plugins/bmc.py
@@ -1,8 +1,18 @@
 def page_licence(record):
     """
-    To respond to the provider identifier: http://www.plos.com
+    To respond to the provider identifier: http://www.biomedcentral.com
     
     This should determine the licence conditions of the BMC article and populate
     the record['bibjson']['license'] (note the US spelling) field.
     """
-    pass
+    # 1. get content
+    import requests
+    r = requests.get(record['provider'])
+    
+    # 2. see if required string is in content 
+    test_for = "This is an Open Access article distributed under the terms of the Creative Commons Attribution License (<a href='http://creativecommons.org/licenses/by/2.0'>http://creativecommons.org/licenses/by/2.0</a>), which permits unrestricted use, distribution, and reproduction in any medium, provided the original work is properly cited."
+
+    # 3. return appropriate license.
+    if test_for in r.content:
+        record['bibjson']['license'] = 'CC-BY 2.0'
+
