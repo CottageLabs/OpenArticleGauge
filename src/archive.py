f4407daf4af3cafb029a5938f34f2f9987f953f7
Richard Jones <richard@cottagelabs.com>
2013-02-07 19:40:42 +0000

    add model serialisation to json and model tests

--- src/archive.py
+++ src/archive.py
@@ -1,6 +1,7 @@
 def check_archive(identifier):
     """
-    Check the archive layer for an object with the given identifier.
+    Check the archive layer for an object with the given (canonical) identifier,
+    which can be found in the bibjson['identifier']['canonical'] field
     
     Return a bibjson record
     """
@@ -8,6 +9,7 @@ def check_archive(identifier):
     
 def store(bibjson):
     """
-    Store the provided bibjson record in the archive
+    Store the provided bibjson record in the archive (overwriting any item which
+    has the same canonical identifier)
     """
     pass
