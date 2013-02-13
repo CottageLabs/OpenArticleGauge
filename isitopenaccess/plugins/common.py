8efb64aacb422b0fc3549ea36988fbcbdf9c2ef2
Emanuil Tolev <emanuil.tolev@gmail.com>
2013-02-13 21:20:27 +0000

    move some plugin functionality out to a common.py file; update BMC plugin and associated tests

---
+++ isitopenaccess/plugins/common.py
@@ -0,0 +1,2 @@
+def gen_provenance_description(source_url, statement):
+    return 'License decided by scraping the resource at ' + source_url + ' and looking for the following license statement: "' + statement + '".'
