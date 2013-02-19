2b1cd9a58aa51ef53996dc1897a7a0e50f29d7ca
Emanuil Tolev <emanuil.tolev@gmail.com>
2013-02-19 14:15:17 +0000

    ADD MISSING FILE TO PREV COMMIT "modify BMC plugin: overwrite URL for CC-BY license. We have a MORE specific URL (from the license statement on the BMC pages) than the Open Definition one"

--- isitopenaccess/plugins/bmc.py
+++ isitopenaccess/plugins/bmc.py
@@ -18,7 +18,9 @@ def page_license(record):
     # and meaning['version'] identifies the license version (if available)
     lic_statements = [
         {"This is an Open Access article distributed under the terms of the Creative Commons Attribution License (<a href='http://creativecommons.org/licenses/by/2.0'>http://creativecommons.org/licenses/by/2.0</a>), which permits unrestricted use, distribution, and reproduction in any medium, provided the original work is properly cited.":
-            {'type': 'cc-by', 'version':'2.0', 'open_access': True, 'BY': True, 'NC': False, 'SA': False, 'ND': False}
+            {'type': 'cc-by', 'version':'2.0', 'open_access': True, 'BY': True, 'NC': False, 'SA': False, 'ND': False,
+                # also declare some properties which override info about this license in the licenses list (see licenses module)
+                'url': 'http://creativecommons.org/licenses/by/2.0'}
         }
     ]
 
