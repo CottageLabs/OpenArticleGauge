c59515e47bcfd5d28a0745f27a45c361c581bb4d
Emanuil Tolev <emanuil.tolev@gmail.com>
2013-02-19 14:55:04 +0000

    plugins need to generate A LIST of licenses, not a single license dict

--- isitopenaccess/tests/test_bmc.py
+++ isitopenaccess/tests/test_bmc.py
@@ -26,30 +26,31 @@ class TestWorkflow(TestCase):
 
         # check if all the important keys were created
         assert record['bibjson'].has_key('license')
+        assert record['bibjson']['license']
 
         # NB: some examples may fail the 'url' test since the Open Definition
         # data we're using as the basis for our licenses dictionary does not
         # have 'url' for all licenses. Fix by modifying licenses.py - add the data.
-        assert all (key in record['bibjson']['license'] for key in keys_in_license)
+        assert all (key in record['bibjson']['license'][-1] for key in keys_in_license)
 
-        assert all (key in record['bibjson']['license']['provenance'] for key in keys_in_provenance)
+        assert all (key in record['bibjson']['license'][-1]['provenance'] for key in keys_in_provenance)
 
         # some content checks now
-        assert record['bibjson']['license']['type'] == 'cc-by'
-        assert record['bibjson']['license']['version'] == '2.0'
-        assert 'id' not in record['bibjson']['license'] # should not have "id" - due to bibserver
-        assert not record['bibjson']['license']['jurisdiction']
-        assert record['bibjson']['license']['open_access']
-        assert record['bibjson']['license']['BY']
-        assert not record['bibjson']['license']['NC']
-        assert not record['bibjson']['license']['SA']
-        assert not record['bibjson']['license']['ND']
+        assert record['bibjson']['license'][-1]['type'] == 'cc-by'
+        assert record['bibjson']['license'][-1]['version'] == '2.0'
+        assert 'id' not in record['bibjson']['license'][-1] # should not have "id" - due to bibserver
+        assert not record['bibjson']['license'][-1]['jurisdiction']
+        assert record['bibjson']['license'][-1]['open_access']
+        assert record['bibjson']['license'][-1]['BY']
+        assert not record['bibjson']['license'][-1]['NC']
+        assert not record['bibjson']['license'][-1]['SA']
+        assert not record['bibjson']['license'][-1]['ND']
         # In this case we also expect the BMC plugin to overwrite the ['license']['url']
         # property with a more specific one from the license statement.
-        assert record['bibjson']['license']['url'] == 'http://creativecommons.org/licenses/by/2.0'
+        assert record['bibjson']['license'][-1]['url'] == 'http://creativecommons.org/licenses/by/2.0'
 
-        assert record['bibjson']['license']['provenance']['agent'] == config.agent
-        assert record['bibjson']['license']['provenance']['source'] == record['provider']['url']
-        assert record['bibjson']['license']['provenance']['date']
-        assert record['bibjson']['license']['provenance']['category'] == 'page_scrape'
-        assert record['bibjson']['license']['provenance']['description'] == 'License decided by scraping the resource at http://www.biomedcentral.com/1471-2164/13/425 and looking for the following license statement: "This is an Open Access article distributed under the terms of the Creative Commons Attribution License (<a href=\'http://creativecommons.org/licenses/by/2.0\'>http://creativecommons.org/licenses/by/2.0</a>), which permits unrestricted use, distribution, and reproduction in any medium, provided the original work is properly cited.".'
+        assert record['bibjson']['license'][-1]['provenance']['agent'] == config.agent
+        assert record['bibjson']['license'][-1]['provenance']['source'] == record['provider']['url']
+        assert record['bibjson']['license'][-1]['provenance']['date']
+        assert record['bibjson']['license'][-1]['provenance']['category'] == 'page_scrape'
+        assert record['bibjson']['license'][-1]['provenance']['description'] == 'License decided by scraping the resource at http://www.biomedcentral.com/1471-2164/13/425 and looking for the following license statement: "This is an Open Access article distributed under the terms of the Creative Commons Attribution License (<a href=\'http://creativecommons.org/licenses/by/2.0\'>http://creativecommons.org/licenses/by/2.0</a>), which permits unrestricted use, distribution, and reproduction in any medium, provided the original work is properly cited.".'
