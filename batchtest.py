586dfe5f9b45436432bc68d328f7ab879ae9f96f
markmacgillivray <mark@cottagelabs.com>
2013-03-05 00:45:05 +0000

    adding batchtest and t.json, slight edit to search page

---
+++ batchtest.py
@@ -0,0 +1,46 @@
+import requests, json
+from time import sleep
+
+
+def batchtest():
+    # get some records from the OCC corpus (this is a test server, address may change)
+    r = requests.get('http://129.67.24.26:9200/test/record/_search?q=journal.name:plos* OR journal.name:bmc*&size=5')
+
+    # get a list of IDs from the records pulled
+    # this may be longer than the amount of records, cos records can have more than one ID type
+    ids = []
+    for rs in r.json().get('hits',{}).get('hits',[]):
+        if len(ids) != 1000:
+            rec = rs['_source']
+            for i in rec.get('identifier',[]):
+                if 'type' in i and 'id' in i:
+                    if i['type'].lower() in ["doi","pmid"]:
+                        ids.append(i['id'])
+
+    # send the list to the IIOA service
+    # loop it until informed they have all been processed
+    while len(ids):
+        print len(ids)
+        headers = {'content-type': 'application/json'}
+        rr = requests.post('http://iioa.cottagelabs.com/lookup/',data=json.dumps(ids), headers=headers)
+        rs = rr.json()
+        print json.dumps(rs,indent=4)
+        if len(rs['processing']) == 0:
+            return
+        else:
+            # strip found from ids and wait then try again
+            ids = []
+            for p in rs['processing']:
+                ids.append(p['identifier']['id']);
+            sleep(2)
+    
+    
+# run and time it
+if __name__ == "__main__":
+    from datetime import datetime
+    started = datetime.now()
+    print started
+    batchtest()
+    ended = datetime.now()
+    print ended
+    print ended - started
