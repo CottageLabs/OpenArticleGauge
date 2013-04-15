ab796fe0f60c19fd40b2f1c49a909a12f50f01d6
Cameron Neylon <cn@cameronneylon.net>
2013-04-15 13:44:01 +0100

    Added tested plugin for Nature properties including Scientific Reports

---
+++ openarticlegauge/plugins/nature.py
@@ -0,0 +1,86 @@
+"""
+This plugin handles Nature articles, including Scientific Reports and Nature Communications
+"""
+
+from openarticlegauge import plugin
+
+class NaturePlugin(plugin.Plugin):
+    _short_name = "nature"
+    __version__='0.1' # consider incrementing or at least adding a minor version
+                    # e.g. "0.1.1" if you change this plugin
+
+    # The domains that this plugin will say it can support.
+    # Specified without the schema (protocol - e.g. "http://") part.
+    base_urls = ["www.nature.com/"]
+    
+    # so if the http://www.nature.com/ncomms/journal/v1/n1/full/ncomms1007.html URL comes in,
+    # it should be supported.
+    
+    def supports(self, provider):
+        """
+        Does this plugin support this provider
+        """
+        
+        work_on = self.clean_urls(provider.get("url", []))
+
+        for url in work_on:
+            if self.supports_url(url):
+                return True
+
+        return False
+
+    def supports_url(self, url):
+        """
+        Same as the supports() function but answers the question for a single URL.
+        """
+        for bu in self.base_urls:
+            if self.clean_url(url).startswith(bu):
+                return True
+        return False
+
+    def license_detect(self, record):        
+        """
+        To respond to the provider identifier: http://www.nature.com/
+        
+        This should determine the licence conditions of the Nature article and populate
+        the record['bibjson']['license'] (note the US spelling) field.
+        """
+
+        # licensing statements to look for on this publisher's pages
+        # take the form of {statement: meaning}
+
+        ## ~~ TUTORIAL: YOU NEED TO MODIFY THIS ~~
+        lic_statements = [
+            {'This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. To view a copy of this license, visit  <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/">http://creativecommons.org/licenses/by-nc-sa/3.0/</a>':
+                {'type': 'cc-nc-sa', # license type, see the licenses module for available ones
+                         'version':'3.0', # version of the license if specified, can be blank
+                         'url': 'http://creativecommons.org/licenses/by-nc-sa/3.0/'}
+            },
+            {'This work is licensed under a Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License. To view a copy of this license, visit  <a href="http://creativecommons.org/licenses/by-nc-nd/3.0/">http://creativecommons.org/licenses/by-nc-nd/3.0/</a>':
+                {'type': 'cc-nc-nd', # license type, see the licenses module for available ones
+                         'version':'3.0', # version of the license if specified, can be blank
+                         'url': 'http://creativecommons.org/licenses/by-nc-nd/3.0/'}
+            },             
+            {'This work is licensed under a Creative Commons Attribution 3.0 Unported License. To view a copy of this license, visit  <a href="http://creativecommons.org/licenses/by/3.0/">http://creativecommons.org/licenses/by/3.0/</a>':
+                {'type': 'cc-by', # license type, see the licenses module for available ones
+                         'version':'3.0', # version of the license if specified, can be blank
+                         'url': 'http://creativecommons.org/licenses/by/3.0/'}
+            },              
+            {'<div class="subscribe-prompt" id="access">':
+                {'type': 'NPG-copyright-transfer', # license type, see the licenses module for available ones
+                 'version': '', # version of the license if specified, can be blank
+                 }
+            } 
+        ]
+        
+        # some basic protection against missing fields in the incoming record
+        if "provider" not in record:
+            return
+        if "url" not in record["provider"]:
+            return
+        
+        # For all URL-s associated with this resource...
+        for url in record['provider']['url']:
+            # ... run the dumb string matcher if the URL is supported.
+            if self.supports_url(url):
+                self.simple_extract(lic_statements, record, url)
---
+++ openarticlegauge/tests/test_nature.py
@@ -0,0 +1,254 @@
+# FIXME: do we still need this test case, if test_provider_skeleton is working fine?
+
+from unittest import TestCase
+
+from openarticlegauge.plugins.nature import NaturePlugin # TUTORIAL: change this to import *your* plugin
+from openarticlegauge import config
+
+# TUTORIAL: no need to modify any of this unless you added a key to the license info
+keys_in_license = ['provenance', 'description', 'type', 'title', 'url',
+    'jurisdiction', 'open_access', 'BY', 'NC', 'SA', 'ND']
+
+keys_in_provenance = ['date', 'agent', 'source', 'category', 'description']
+
+class TestBasic(TestCase):
+
+    def setUp(self):
+        pass
+        
+    def tearDown(self):
+        pass
+    
+    # TUTORIAL
+    # Give some examples of dereferenced URL-s which you expect to work.
+    # Dereferenced means NOT http://dx.doi.org/10.1186/1471-2164-13-425
+    # but the result of the redirect from hitting that).
+    def test_01_nature_supports_success(self):
+        test_urls = ["http://www.nature.com/ncomms/journal/v1/n1/full/ncomms1007.html"]
+        npg = NaturePlugin()
+        for url in test_urls:
+            assert npg.supports({"url" : [url]})
+
+    # TUTORIAL
+    # Now give some examples of URL-s and strings that should not be
+    # supported.        
+    def test_02_nature_supports_fail(self):
+        test_urls = ["http://www.plosone.org/", "askjdfsakjdhfsa"]
+        npg = NaturePlugin()
+        for url in test_urls:
+            assert not npg.supports({"url" : [url]})
+
+    # TUTORIAL: Repeat success examples from above test. 
+    def test_03_npg_supports_url_success(self):
+        test_urls = ["http://www.nature.com/srep/2013/130415/srep01657/full/srep01657.html"]
+        npg = NaturePlugin()
+        for url in test_urls:
+            assert npg.supports_url(url)
+    
+    # TUTORIAL: Repeat failure examples from above test. 
+    def test_04_npg_supports_url_fail(self):
+        npg = NaturePlugin()
+        test_urls = ["http://www.biomedcentral.com/983242", "askjdfsakjdhfsa"]
+        for url in test_urls:
+            assert not npg.supports_url(url)
+
+    def test_05_name_and_version(self):
+        """
+        Take an example supported article and check just the handler fields
+        """
+        record = {}
+        record['bibjson'] = {}
+        record['provider'] = {}
+        record['provider']['url'] = ['http://www.nature.com/srep/2013/130415/srep01657/full/srep01657.html']
+        
+        npg = NaturePlugin()
+        npg.license_detect(record)
+
+        # just barebones checks to make sure the license and provenance objects
+        # exist in the first place so the handler fields can be checked
+        assert record['bibjson'].has_key('license')
+        assert record['bibjson']['license']
+
+        assert 'provenance' in record['bibjson']['license'][-1]
+
+        assert 'handler' in record['bibjson']['license'][-1]['provenance']
+        assert record['bibjson']['license'][-1]['provenance']['handler'] == npg._short_name
+        assert record['bibjson']['license'][-1]['provenance']['handler_version'] == npg.__version__
+    
+    def test_06_npg_ccncsa_license(self):
+        record = {}
+        record['bibjson'] = {}
+        record['provider'] = {}
+        
+        # TUTORIAL
+        # Again, you must provide a dereferenced URL here - that's what your
+        # plugin will get!
+        record['provider']['url'] = ['http://www.nature.com/srep/2013/130415/srep01657/full/srep01657.html']
+
+        npg = NaturePlugin()
+        npg.license_detect(record)
+
+        # check if all the important keys were created
+        
+        # TUTORIAL: You don't need to modify any of these
+        assert record['bibjson'].has_key('license')
+        assert record['bibjson']['license']
+
+        # NB: some examples may fail the 'url' test since the Open Definition
+        # data we're using as the basis for our licenses dictionary does not
+        # have 'url' for all licenses. Fix by modifying licenses.py - add the data.
+        assert all (key in record['bibjson']['license'][-1] for key in keys_in_license)
+
+        assert all (key in record['bibjson']['license'][-1]['provenance'] for key in keys_in_provenance)
+
+        # some content checks now
+        # TUTORIAL: this is what you need to modify to make sure your plugin is
+        # recording the right data. Essentially this should match whatever you put
+        # into lic_statements in the plugin code and whatever you're expecting
+        # for this particular resource.
+        assert record['bibjson']['license'][-1]['type'] == 'cc-nc-sa'
+        assert record['bibjson']['license'][-1]['version'] == '3.0'
+        assert 'id' not in record['bibjson']['license'][-1] # should not have "id" - due to bibserver
+        assert not record['bibjson']['license'][-1]['jurisdiction']
+        assert not record['bibjson']['license'][-1]['open_access']
+        assert record['bibjson']['license'][-1]['BY']
+        assert record['bibjson']['license'][-1]['NC']
+        assert record['bibjson']['license'][-1]['SA']
+        assert not record['bibjson']['license'][-1]['ND']
+        # In this case we also expect the plugin to overwrite the ['license']['url']
+        # property with a more specific one from the license statement.
+        assert record['bibjson']['license'][-1]['url'] == 'http://creativecommons.org/licenses/by-nc-sa/3.0/'
+
+        # TUTORIAL: you don't need to touch the following tests, EXCEPT for
+        # the last one - the human-readable provenance description.
+        assert record['bibjson']['license'][-1]['provenance']['agent'] == config.agent
+        assert record['bibjson']['license'][-1]['provenance']['source'] == record['provider']['url'][0]
+        assert record['bibjson']['license'][-1]['provenance']['date']
+        assert record['bibjson']['license'][-1]['provenance']['category'] == 'page_scrape'
+
+        # TUTORIAL: This is what you need to change - this is the license
+        # statement from lic_statements that you expect to have been present
+        # on this resource's page.
+        lic_statement = 'This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. To view a copy of this license, visit  <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/">http://creativecommons.org/licenses/by-nc-sa/3.0/</a>' \
+        
+        # TUTORIAL: this is essentially some boilerplate text that you should not change
+        assert record['bibjson']['license'][-1]['provenance']['description'] == 'License decided by scraping the resource at ' + record['provider']['url'][0] + ' and looking for the following license statement: "' + lic_statement + '".'
+        
+    def test_07_npg_ccncnd_license(self):
+        record = {}
+        record['bibjson'] = {}
+        record['provider'] = {}
+        
+        # TUTORIAL
+        # Again, you must provide a dereferenced URL here - that's what your
+        # plugin will get!
+        record['provider']['url'] = ['http://www.nature.com/ncomms/journal/v4/n4/full/ncomms2674.html']
+
+        npg = NaturePlugin()
+        npg.license_detect(record)
+
+        # check if all the important keys were created
+        
+        # TUTORIAL: You don't need to modify any of these
+        assert record['bibjson'].has_key('license')
+        assert record['bibjson']['license']
+
+        # NB: some examples may fail the 'url' test since the Open Definition
+        # data we're using as the basis for our licenses dictionary does not
+        # have 'url' for all licenses. Fix by modifying licenses.py - add the data.
+        assert all (key in record['bibjson']['license'][-1] for key in keys_in_license)
+
+        assert all (key in record['bibjson']['license'][-1]['provenance'] for key in keys_in_provenance)
+
+        # some content checks now
+        # TUTORIAL: this is what you need to modify to make sure your plugin is
+        # recording the right data. Essentially this should match whatever you put
+        # into lic_statements in the plugin code and whatever you're expecting
+        # for this particular resource.
+        assert record['bibjson']['license'][-1]['type'] == 'cc-nc-nd'
+        assert record['bibjson']['license'][-1]['version'] == '3.0'
+        assert 'id' not in record['bibjson']['license'][-1] # should not have "id" - due to bibserver
+        assert not record['bibjson']['license'][-1]['jurisdiction']
+        assert not record['bibjson']['license'][-1]['open_access']
+        assert record['bibjson']['license'][-1]['BY']
+        assert record['bibjson']['license'][-1]['NC']
+        assert not record['bibjson']['license'][-1]['SA']
+        assert record['bibjson']['license'][-1]['ND']
+        # In this case we also expect the plugin to overwrite the ['license']['url']
+        # property with a more specific one from the license statement.
+        assert record['bibjson']['license'][-1]['url'] == 'http://creativecommons.org/licenses/by-nc-nd/3.0/'
+
+        # TUTORIAL: you don't need to touch the following tests, EXCEPT for
+        # the last one - the human-readable provenance description.
+        assert record['bibjson']['license'][-1]['provenance']['agent'] == config.agent
+        assert record['bibjson']['license'][-1]['provenance']['source'] == record['provider']['url'][0]
+        assert record['bibjson']['license'][-1]['provenance']['date']
+        assert record['bibjson']['license'][-1]['provenance']['category'] == 'page_scrape'
+
+        # TUTORIAL: This is what you need to change - this is the license
+        # statement from lic_statements that you expect to have been present
+        # on this resource's page.
+        lic_statement = 'This work is licensed under a Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License. To view a copy of this license, visit  <a href="http://creativecommons.org/licenses/by-nc-nd/3.0/">http://creativecommons.org/licenses/by-nc-nd/3.0/</a>' \
+        
+        # TUTORIAL: this is essentially some boilerplate text that you should not change
+        assert record['bibjson']['license'][-1]['provenance']['description'] == 'License decided by scraping the resource at ' + record['provider']['url'][0] + ' and looking for the following license statement: "' + lic_statement + '".'
+        
+    def test_08_npg_ccby_license(self):
+        record = {}
+        record['bibjson'] = {}
+        record['provider'] = {}
+        
+        # TUTORIAL
+        # Again, you must provide a dereferenced URL here - that's what your
+        # plugin will get!
+        record['provider']['url'] = ['http://www.nature.com/srep/2013/130129/srep01154/full/srep01154.html']
+
+        npg = NaturePlugin()
+        npg.license_detect(record)
+
+        # check if all the important keys were created
+        
+        # TUTORIAL: You don't need to modify any of these
+        assert record['bibjson'].has_key('license')
+        assert record['bibjson']['license']
+
+        # NB: some examples may fail the 'url' test since the Open Definition
+        # data we're using as the basis for our licenses dictionary does not
+        # have 'url' for all licenses. Fix by modifying licenses.py - add the data.
+        assert all (key in record['bibjson']['license'][-1] for key in keys_in_license)
+
+        assert all (key in record['bibjson']['license'][-1]['provenance'] for key in keys_in_provenance)
+
+        # some content checks now
+        # TUTORIAL: this is what you need to modify to make sure your plugin is
+        # recording the right data. Essentially this should match whatever you put
+        # into lic_statements in the plugin code and whatever you're expecting
+        # for this particular resource.
+        assert record['bibjson']['license'][-1]['type'] == 'cc-by'
+        assert record['bibjson']['license'][-1]['version'] == '3.0'
+        assert 'id' not in record['bibjson']['license'][-1] # should not have "id" - due to bibserver
+        assert not record['bibjson']['license'][-1]['jurisdiction']
+        assert record['bibjson']['license'][-1]['open_access']
+        assert record['bibjson']['license'][-1]['BY']
+        assert not record['bibjson']['license'][-1]['NC']
+        assert not record['bibjson']['license'][-1]['SA']
+        assert not record['bibjson']['license'][-1]['ND']
+        # In this case we also expect the plugin to overwrite the ['license']['url']
+        # property with a more specific one from the license statement.
+        assert record['bibjson']['license'][-1]['url'] == 'http://creativecommons.org/licenses/by/3.0/'
+
+        # TUTORIAL: you don't need to touch the following tests, EXCEPT for
+        # the last one - the human-readable provenance description.
+        assert record['bibjson']['license'][-1]['provenance']['agent'] == config.agent
+        assert record['bibjson']['license'][-1]['provenance']['source'] == record['provider']['url'][0]
+        assert record['bibjson']['license'][-1]['provenance']['date']
+        assert record['bibjson']['license'][-1]['provenance']['category'] == 'page_scrape'
+
+        # TUTORIAL: This is what you need to change - this is the license
+        # statement from lic_statements that you expect to have been present
+        # on this resource's page.
+        lic_statement = 'This work is licensed under a Creative Commons Attribution 3.0 Unported License. To view a copy of this license, visit  <a href="http://creativecommons.org/licenses/by/3.0/">http://creativecommons.org/licenses/by/3.0/</a>' \
+        
+        # TUTORIAL: this is essentially some boilerplate text that you should not change
+        assert record['bibjson']['license'][-1]['provenance']['description'] == 'License decided by scraping the resource at ' + record['provider']['url'][0] + ' and looking for the following license statement: "' + lic_statement + '".'
+        
