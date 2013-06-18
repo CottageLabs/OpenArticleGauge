b33b4e3000149a75ea89bc4f1b9ef65bfc8a40a3
Emanuil Tolev <emanuil.tolev@gmail.com>
2013-06-18 11:10:21 +0100

    added a little too much info in prev. commit

--- openarticlegauge/workflow.py
+++ openarticlegauge/workflow.py
@@ -28,7 +28,7 @@ from openarticlegauge import models, model_exceptions, config, cache, plugin, re
 import logging
 from openarticlegauge.slavedriver import celery
 
-LOG_FORMAT = '%(asctime)-15s %(clientip)s %(message)s'
+LOG_FORMAT = '%(asctime)-15s %(message)s'
 logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
 log = logging.getLogger(__name__)
 
