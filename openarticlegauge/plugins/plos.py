3cbac5effa3a35f4416c66a97fe9f5aa8011f484
Emanuil Tolev <emanuil.tolev@gmail.com>
2013-11-19 15:25:57 +0000

    add new form of CC-BY statement to PLoS plugin

--- openarticlegauge/plugins/plos.py
+++ openarticlegauge/plugins/plos.py
@@ -48,6 +48,9 @@ class PLOSPlugin(plugin.Plugin):
             {"This is an open-access article distributed under the terms of the Creative Commons Attribution License, which permits unrestricted use, distribution, and reproduction in any medium, provided the original author and source are credited.":
                 {'type': 'cc-by'}
             },
+            {"This is an open-access article distributed under the terms of the Creative Commons Attribution License, which permits unrestricted use, distribution, and reproduction in any medium, provided the original work is properly cited.":
+                {'type': 'cc-by'}
+            },
             {"This is an Open Access article in the spirit of the Public Library of Science (PLoS) principles for Open Access http://www.plos.org/oa/, without any waiver of WHO's privileges and immunities under international law, convention, or agreement. This article should not be reproduced for use in association with the promotion of commercial products, services, or any legal entity. There should be no suggestion that WHO endorses any specific organization or products. The use of the WHO logo is not permitted. This notice should be preserved along with the article's original URL.":
                 {'type': 'plos-who'}
             },
