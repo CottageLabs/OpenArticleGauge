4b0637b257d8cb0d1c561864df897cf046ff433f
Emanuil Tolev <emanuil.tolev@gmail.com>
2014-02-28 09:47:00 +0000

    register a publisher form - remove buttons

--- openarticlegauge/app.py
+++ openarticlegauge/app.py
@@ -11,11 +11,11 @@ from openarticlegauge.view.issue import blueprint as issue
 from openarticlegauge.view.lookup import blueprint as lookup
 from openarticlegauge.view.account import blueprint as account
 from openarticlegauge.view.admin import blueprint as admin
-from openarticlegauge.view.license_form import blueprint as license_form
+from openarticlegauge.view.publisher import blueprint as publisher
 from openarticlegauge.view.resolve_doi import blueprint as resolve_doi
 
 app.register_blueprint(contact, url_prefix='/contact')
-app.register_blueprint(license_form, url_prefix='/license_statement')
+app.register_blueprint(publisher, url_prefix='/publisher')
 app.register_blueprint(query, url_prefix='/query')
 app.register_blueprint(issue, url_prefix='/issue')
 app.register_blueprint(account, url_prefix="/account")
---
+++ openarticlegauge/view/publisher.py
@@ -0,0 +1,55 @@
+import json
+from flask.ext.wtf import Form
+from wtforms import TextField, BooleanField, TextAreaField, SelectField, validators, FormField, FieldList
+from wtforms.widgets import TableWidget
+from wtforms.validators import Required, Length
+from flask import Blueprint, request, redirect, url_for, abort, render_template, flash
+
+from openarticlegauge.models import Publisher
+from openarticlegauge.licenses import licenses_dropdown
+
+blueprint = Blueprint('publisher', __name__)
+
+
+@blueprint.route('/', methods=['GET'])
+@blueprint.route('/list', methods=['GET'])
+def list_publishers():
+    return render_template('publishers.html', publishers=Publisher.all())
+
+@blueprint.route('/new', methods=['GET','POST'])
+@blueprint.route('/<publisher_id>', methods=['GET','POST'])
+def publisher_edit(publisher_id=None):
+    p = Publisher.pull(publisher_id)
+    form = PublisherLicenseForm(request.form, p)
+       
+    print json.dumps(form.data, indent=4)
+    if request.method == 'POST' and form.validate():
+        if not p:
+            p = Publisher()
+        p.publisher_name = form.publisher_name.data
+        p.journal_urls = form.journal_urls.data
+        p.licenses = form.licenses.data
+        #form.populate_obj(p)
+        print json.dumps(p.data, indent=4)
+        p.save()
+        return redirect(url_for('.publisher_edit', publisher_id=p.id))
+        
+    return render_template('publisher.html', 
+            form=form)
+
+class LicenseForm(Form): 
+
+    def __init__(self, csrf_enabled=False, *args, **kwargs):
+        super(LicenseForm, self).__init__(csrf_enabled=csrf_enabled, *args, **kwargs)
+    
+    license_statement = TextAreaField('License Statement', [validators.required()])
+    license_type = SelectField('Licenses', [validators.required()], choices=licenses_dropdown)
+    version = TextField('Version')
+    example_doi = TextField('Example DOI', [validators.required(), validators.Regexp("^((http:\/\/){0,1}dx.doi.org/|(http:\/\/){0,1}hdl.handle.net\/|doi:|info:doi:){0,1}(?P<id>10\..+\/.+)")])
+            
+class PublisherLicenseForm(Form):
+    publisher_name = TextField('Publisher Name', [validators.required()])
+    journal_urls = FieldList(TextField('Journal URL',[validators.required(), validators.URL()]), min_entries=1)
+    licenses = FieldList(FormField(LicenseForm), min_entries=1)
+    
+
