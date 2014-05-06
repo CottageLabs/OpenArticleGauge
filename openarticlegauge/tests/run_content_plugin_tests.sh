40637befba611c6b712be94ef326439ffa7c1ccd
Emanuil Tolev <emanuil.tolev@gmail.com>
2014-05-06 08:24:04 +0100

    add newest plugin tests to convenience runner

--- openarticlegauge/tests/run_content_plugin_tests.sh
+++ openarticlegauge/tests/run_content_plugin_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # If you are running in a virtualenv, activate it before invoking this script.
 # Also update the script if you add any tests for content plugins.
-nosetests test_generic_string_matcher.py test_bmc.py test_cell_reports.py test_copernicus.py test_elife.py test_hindawi.py test_nature.py test_oup.py
+nosetests test_generic_string_matcher.py test_bmc.py test_bmj.py test_cell_reports.py test_copernicus.py test_elife.py test_hindawi.py test_nature.py test_oup.py test_sage.py test_sciencedirect.py test_springerlink.py
