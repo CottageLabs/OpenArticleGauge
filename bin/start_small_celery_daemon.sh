cbe5686b4f064fd7f5740220796e1da845c9a990
Cottage Labs LLP <us@cottagelabs.com>
2014-09-18 10:47:07 +0100

    scale down celery a bit more

--- bin/start_small_celery_daemon.sh
+++ bin/start_small_celery_daemon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-celery multi start 8 -A openarticlegauge.slavedriver -l info --concurrency=4 --pidfile=%n.pid --logfile=%n.log -Q:1 detect_provider -Q:2-5 provider_licence -Q:6 store_results -Q:7 flush_buffer -Q:8 priority_detect_provider,priority_provider_licence,priority_store_results
+#celery multi start 8 -A openarticlegauge.slavedriver -l info --concurrency=4 --pidfile=%n.pid --logfile=%n.log -Q:1 detect_provider -Q:2-5 provider_licence -Q:6 store_results -Q:7 flush_buffer -Q:8 priority_detect_provider,priority_provider_licence,priority_store_results
+celery multi start 6 -A openarticlegauge.slavedriver -l info --concurrency=4 --pidfile=%n.pid --logfile=%n.log -Q:1 detect_provider -Q:2-3 provider_licence -Q:4 store_results -Q:5 flush_buffer -Q:6 priority_detect_provider,priority_provider_licence,priority_store_results
 
 # start a celery beat instance which will publish flush_buffer requests
 # to the flush_buffer queue (managed by Worker 8 above)
