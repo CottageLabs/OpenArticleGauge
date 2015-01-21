6eee2cbb4aa639905f280fb4a373d7600fc38a25
Emanuil Tolev <emanuil.tolev@gmail.com>
2015-01-21 18:08:19 +0000

    add celery beat command line options

--- bin/start_celery_in_cmdline.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run celery in a commandline window to monitor it
-# Running in 'screen' might be an idea...
-celery worker --app=openarticlegauge.slavedriver -B -l info -Q detect_provider,provider_licence,store_results,flush_buffer,priority_detect_provider,priority_provider_licence,priority_store_results
-
-# (Of course there is the daemonised way to run it.)
